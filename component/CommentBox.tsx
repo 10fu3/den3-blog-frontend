f440b4b93f2c007aaa71df68c78119000cfdade2
10fu3 <msf.1192@gmail.com>
2022-01-22 00:48:10 +0900

    commit

--- component/CommentBox.tsx
+++ component/CommentBox.tsx
@@ -1,8 +1,8 @@
 import React, {useState} from "react";
-import {BlogContentComment} from "../../old_frontend/component/blog/BlogContent";
 import {Divider, HStack, Spacer, Text, VStack} from "@chakra-ui/layout";
 import {ListItem} from "./List";
 import {Button} from "@chakra-ui/button";
+import {BlogContentComment} from "../model/BlogContentEntity";
 
 function calcTextAreaHeight(value:string){
     let rowsNum = value.split('\n').length;
